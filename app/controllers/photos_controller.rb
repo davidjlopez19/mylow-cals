0164005c3c4e11294b7e3dd43ab5d3914cbbbe9a
David Lopez <davidjlopez@comcast.net>
2019-05-15 00:13:53 +0000

    Setup SendGrid for the Production environment

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,12 +4,23 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
+    # @photo = Photo.new
+    # @photo.user_id = current_user.id
+    # @photo.place_id = @place.id
+    # @photo.picture = params[:picture]
+    # @photo.caption = params[:caption]
+    # @photo.save!
+    # puts @photo.errors.inspect
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:picture)
+    params.require(:photo).permit(:picture, :caption)
+  end
+
+  def extension_white_list
+  %w(jpg jpeg gif png)
   end
 end
