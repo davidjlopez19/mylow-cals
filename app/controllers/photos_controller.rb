44334bc06a7e8c635d9fce47c02a74f084377efe
David Lopez <davidjlopez@comcast.net>
2019-05-17 04:43:01 +0000

    Modified the photo_controller.rb file

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,13 +4,6 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
-    # @photo = Photo.new
-    # @photo.user_id = current_user.id
-    # @photo.place_id = @place.id
-    # @photo.picture = params[:picture]
-    # @photo.caption = params[:caption]
-    # @photo.save!
-    # puts @photo.errors.inspect
     redirect_to place_path(@place)
   end
 
--- app/models/add_userid_to_photo.rb
+++
@@ -1,2 +0,0 @@
-class AddUseridToPhoto < ApplicationRecord
-end
